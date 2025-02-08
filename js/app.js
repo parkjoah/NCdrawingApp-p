f69b89c0f3b1b8ff01d21dbf309a71e028836eb8
조아 <parkjoah@naver.com>
2025-02-08 23:43:34 +0900

    feat : 수정

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraseBtn = document.getElementById("erase-btn");
 const colorOptions = Array.from(
-    document.getElementsByClassName("color-option")
+  document.getElementsByClassName("color-option")
 );
 const canvas = document.querySelector("canvas");
 
@@ -13,118 +13,118 @@ const lineWidth = document.getElementById("line-width");
 const color = document.getElementById("color");
 
 const ctx = canvas.getContext("2d");
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 800;
+const CANVAS_WIDTH = 600;
+const CANVAS_HEIGHT = 600;
 
-canvas.width =CANVAS_WIDTH;
-canvas.height=CANVAS_HEIGHT;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 ctx.lineCap = "round";
 
 let isPainting = false;
 let isFilling = false;
 
-function onMove(event){
-    if(isPainting){
-        ctx.lineTo(event.offsetX,event.offsetY);
-        ctx.stroke();
-        return;
-    }
-    ctx.moveTo(event.offsetX,event.offsetY);
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
-function startPainting(){
-    ctx.beginPath();
-    isPainting =true;
+function startPainting() {
+  ctx.beginPath();
+  isPainting = true;
 }
-function cancelPainting(){
-    isPainting =false;
-}
-
-function onLineWidthChange(event){
-    ctx.lineWidth = event.target.value;
+function cancelPainting() {
+  isPainting = false;
 }
 
-function onColorChange(event){
-    ctx.strokeStyle = event.target.value;
-    ctx.fillStyle = event.target.value;
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
 }
 
-function onColorClick(event){
-    const colorValue = event.target.dataset.color;
-    ctx.strokeStyle = colorValue;
-    ctx.fillStyle = colorValue;
-    color.value = colorValue;
+function onColorChange(event) {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
 }
 
-function onModeClick(event){
-    if(isFilling){
-        isFilling = false;
-        modeBtn.innerText = "Fill";
-    }else{
-        isFilling = true;
-        modeBtn.innerText = "Draw"
-    }
-}
-function onCanvasClick(){
-    if(isFilling){
-        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    }
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
 }
 
-function onDestroyClick(){
-    ctx.fillStyle="white";
-    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+function onModeClick(event) {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
 }
 
-function onEraseClick(){
-    ctx.strokeStyle="white";
-    isFilling = false;  
+function onDestroyClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 }
 
-function onFileChange(event){
-    const file = event.target.files[0];
-    const url = URL.createObjectURL(file);
-    const image = new Image()
-    image.src = url;
-    image.onload = function(){
-        ctx.drawImage(image,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-        fileInput.value = null;
-    }
+function onEraseClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
 }
 
-function onDoubleClick(event){
-    const text = textInput.value;
-    if(text !== ""){
-        ctx.save(); //ctx의 현재상태, 스타일, 색상 전부 저장
-        ctx.lineWidth = 1;
-        ctx.font = "68px serif"; //size, font-familly
-        ctx.strokeText(text, event.offsetX, event.offsetY);
-        ctx.restore();
-    }
+function onFileChange(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
 }
 
-function onSaveClick(){
-    const url = canvas.toDataURL();
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "myDrawing.png";
-    a.click();
+function onDoubleClick(event) {
+  const text = textInput.value;
+  if (text !== "") {
+    ctx.save(); //ctx의 현재상태, 스타일, 색상 전부 저장
+    ctx.lineWidth = 1;
+    ctx.font = "68px serif"; //size, font-familly
+    ctx.strokeText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+  }
 }
 
-canvas.addEventListener("dblclick",onDoubleClick);
-canvas.addEventListener("mousemove",onMove);
-canvas.addEventListener("mousedown",startPainting);
-canvas.addEventListener("mouseup",cancelPainting);
-canvas.addEventListener("mouseleave",cancelPainting); // 마우스 떠났을 경우에도 추가
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png";
+  a.click();
+}
 
-canvas.addEventListener("click",onCanvasClick);
-lineWidth.addEventListener("change",onLineWidthChange);
-color.addEventListener("change",onColorChange);
+canvas.addEventListener("dblclick", onDoubleClick);
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting); // 마우스 떠났을 경우에도 추가
 
-colorOptions.forEach(color => color.addEventListener("click",onColorClick));
+canvas.addEventListener("click", onCanvasClick);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
 
-modeBtn.addEventListener("click",onModeClick);
-destroyBtn.addEventListener("click",onDestroyClick);
-eraseBtn.addEventListener("click",onEraseClick);
-fileInput.addEventListener("change",onFileChange);
-saveBtn.addEventListener("click",onSaveClick);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraseBtn.addEventListener("click", onEraseClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
